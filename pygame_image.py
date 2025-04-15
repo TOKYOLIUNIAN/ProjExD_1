0f51b0b2793cdb237c886b41929fea8448eb99ba
TOKYO_LIUNIAN <c0a24274@edu.teu.ac.jp>
2025-04-15 15:54:29 +0900

    实现背景图像右向左滑动

--- pygame_image.py
+++ pygame_image.py
@@ -11,19 +11,19 @@ def main():
     clock  = pg.time.Clock()                       # 设置时钟
     bg_img = pg.image.load("fig/pg_bg.jpg")        # 加载背景图像
     kk_img = pg.image.load("fig/3.png")           # 加载角色图像
-    kk_img = pg.transform.flip(kk_img, True,False) # 缩放角色图像
+    kk_img = pg.transform.flip(kk_img, True,False) # 练习2后半
     tmr = 0                                         # 计时器初始化
 
     while True:
         for event in pg.event.get():               # 处理事件
-            if event.type == pg.QUIT:              # 如果点击关闭窗口
-                return                             # 退出主循环
-
-        screen.blit(bg_img, [0, 0])                # 绘制背景图像
+            if event.type == pg.QUIT:  return             # 如果点击关闭窗口
+                                            # 退出主循环
+        x = tmr
+        screen.blit(bg_img, [-x, 0])                # 绘制背景图像
         screen.blit(kk_img, [300, 200])     
         pg.display.update()                        # 更新显示内容
         tmr += 1                                   
-        clock.tick(200)                             # 控制帧率（每秒10帧）
+        clock.tick(200)   #练习5                          # 控制帧率（每秒10帧）
 
 if __name__ == "__main__":
     pg.init()      # 初始化 pygame 模块
