6b8d94b7ac4986261073af120c74b9cc93fb4ef9
TOKYO_LIUNIAN <c0a24274@edu.teu.ac.jp>
2025-04-15 15:42:23 +0900

    练习2和4

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,8 @@ def main():
     screen = pg.display.set_mode((800, 600))       # 创建窗口，大小800x600
     clock  = pg.time.Clock()                       # 设置时钟
     bg_img = pg.image.load("fig/pg_bg.jpg")        # 加载背景图像
+    kk_img = pg.image.load("fig/3.png")           # 加载角色图像
+    kk_img = pg.transform.flip(kk_img, True,False) # 缩放角色图像
     tmr = 0                                         # 计时器初始化
 
     while True:
@@ -18,9 +20,10 @@ def main():
                 return                             # 退出主循环
 
         screen.blit(bg_img, [0, 0])                # 绘制背景图像
+        screen.blit(kk_img, [300, 200])     
         pg.display.update()                        # 更新显示内容
         tmr += 1                                   
-        clock.tick(10)                             # 控制帧率（每秒10帧）
+        clock.tick(200)                             # 控制帧率（每秒10帧）
 
 if __name__ == "__main__":
     pg.init()      # 初始化 pygame 模块
